449bbba9f9abf63288d25f10263b0a9d03f53e81
konstantin <konstantin@wisebits.com>
2023-12-08 13:46:56 +0200

    0.1.1 optimize MessageData

--- poeagent/models/ws_message.py
+++ poeagent/models/ws_message.py
@@ -1,28 +1,10 @@
 from __future__ import annotations
 
-from typing import List, Optional
-
-from pydantic import BaseModel, Field
+from typing import Optional
 
 from .send_message_data import MessageData, Chat
 
 
-class WsMessage(BaseModel):
-    id: str
-    messageId: int
-    creationTime: int
-    clientNonce: Optional[str]
-    state: str
-    text: str
-    author: str
-    contentType: str
-    sourceType: str
-    attachmentTruncationState: str
-    attachments: List
-    vote: None
-    suggestedReplies: List
-    hasCitations: bool
-    field__isNode: str = Field(..., alias='__isNode')
-    textLengthOnCancellation: None
+class WsMessage(MessageData):
     humanMessage: Optional[MessageData] = None
     chat: Optional[Chat] = None
